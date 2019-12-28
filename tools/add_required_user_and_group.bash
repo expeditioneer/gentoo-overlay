eeeae1070d5d96a2f65b4e5bd8d1dcc7b7547cf9
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 23:57:12 +0100

    bugDE

--- tools/add_required_user_and_group.bash
+++ tools/add_required_user_and_group.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 PORTAGE_UID=250
 PORTAGE_GID=250
@@ -9,4 +9,4 @@ sudo groupadd --gid "${PORTAGE_GID}" portage
 
 sudo useradd --uid "${PORTAGE_UID}" --gid "${PORTAGE_GID}" --home /var/tmp/portage --shell /bin/false portage
 
-sudo usermod --append --groups portage travis
+sudo usermod --append --groups portage "${USER}"
