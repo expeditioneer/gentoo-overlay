fa165c5681ca98ec55ac3ebf69d1f7ec6181672f
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 22:41:30 +0100

    ErMhAgErD

--- tools/setup_portage.sh
+++ tools/setup_portage.sh
@@ -28,11 +28,12 @@ ln --symbolic lib64 "${PORTAGE_ROOT}/usr/lib"
 portage_extracted_directory=${temporary_directory}/portage-archive
 mkdir "${portage_extracted_directory}"
 curl --location --silent "https://github.com/gentoo/portage/archive/portage-${PORTAGE_VERSION}.tar.gz" | tar --extract --gzip --directory="${portage_extracted_directory}" --strip-components=1
-
 cd "${portage_extracted_directory}"
 ./setup.py install -O2 --system-prefix="${PORTAGE_ROOT}/usr" --sysconfdir="${PORTAGE_ROOT}/etc"
 cd "${portage_extracted_directory}/repoman"
 ./setup.py install -O2 --system-prefix="${PORTAGE_ROOT}/usr" --sysconfdir="${PORTAGE_ROOT}/etc"
+mkdir --parents /usr/share/repoman/qa_data
+cp "${portage_extracted_directory}/repoman/cnf/qa_data/qa_data.yaml" /usr/share/repoman/qa_data
 
 rm --recursive --force "${temporary_directory}"
 mkdir --parents "${PORTAGE_ROOT}/usr/lib/portage/cnf/"
