fadf3188a018ddd1541ae02250802efa8fb1c5b9
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 23:20:46 +0100

    narrow sudo

--- tools/add_required_user_and_group.sh
+++ tools/add_required_user_and_group.sh
@@ -3,6 +3,6 @@
 PORTAGE_UID=250
 PORTAGE_GID=250
 
-useradd -uid "${PORTAGE_UID}"  --gid "${PORTAGE_GID}" --user-group --home /var/tmp/portage --shell /bin/false portage
+sudo useradd --uid "${PORTAGE_UID}"  --gid "${PORTAGE_GID}" --user-group --home /var/tmp/portage --shell /bin/false portage
 
-usermod --append --groups portage travis
+sudo usermod --append --groups portage travis
