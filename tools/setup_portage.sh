f79ee89a7e3310a53b0f389ddb07db1569e804da
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 22:45:59 +0100

    added sudo - Yo

--- tools/setup_portage.sh
+++ tools/setup_portage.sh
@@ -33,7 +33,7 @@ cd "${portage_extracted_directory}"
 cd "${portage_extracted_directory}/repoman"
 ./setup.py install -O2 --system-prefix="${PORTAGE_ROOT}/usr" --sysconfdir="${PORTAGE_ROOT}/etc"
 sudo mkdir --parents /usr/share/repoman/qa_data
-cp "${portage_extracted_directory}/repoman/cnf/qa_data/qa_data.yaml" /usr/share/repoman/qa_data
+sudo cp "${portage_extracted_directory}/repoman/cnf/qa_data/qa_data.yaml" /usr/share/repoman/qa_data/
 
 rm --recursive --force "${temporary_directory}"
 mkdir --parents "${PORTAGE_ROOT}/usr/lib/portage/cnf/"
