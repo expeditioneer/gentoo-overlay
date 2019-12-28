d9d65870cc425689a8bc397b2f5075360376c6ff
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 15:17:19 +0100

    set latest portage version

--- tools/get_latest_portage_version.sh
+++ tools/get_latest_portage_version.sh
@@ -7,16 +7,10 @@ if [ -z "${GITHUB_GRAPHQL_QUERY_TOKEN}" ]; then
   exit 1
 fi
 
-LATEST_PORTAGE_VERSION=$(curl \
+curl \
   --header "Content-Type: application/json"\
   --header "Authorization: Bearer ${GITHUB_GRAPHQL_QUERY_TOKEN}" \
   --request POST \
   --data '{
             "query": "{ repository(owner: \"gentoo\", name: \"portage\") { tags: refs(refPrefix: \"refs/tags/\", first: 1, orderBy: { field: TAG_COMMIT_DATE, direction: DESC }) { edges { tag: node { name } } } } }"
-          }' https://api.github.com/graphql | jq -r '.data.repository.tags.edges[].tag.name | split("-")[1]')
-
-echo "Latest Portage Version is: ${LATEST_PORTAGE_VERSION}"
-
-temporary_directory="$(mktemp --directory)"
-
-curl --location --silent "https://github.com/gentoo/portage/archive/portage-${LATEST_PORTAGE_VERSION}.tar.gz" | tar --extract --gzip --directory="${temporary_directory}/portage-archive" --strip-components=1
+          }' https://api.github.com/graphql | jq -r '.data.repository.tags.edges[].tag.name | split("-")[1]'
