9eb9403dfd308c32b8e4b4b06723dbbac913346c
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 16:50:10 +0100

    maybe maybe

--- tools/setup_portage.sh
+++ tools/setup_portage.sh
@@ -7,14 +7,13 @@ if [ -z "${PORTAGE_VERSION}" ]; then
   exit 1
 fi
 
-echo -e "\e[33m Used Portage Version is: ${PORTAGE_VERSION}"
+echo -e "\e[1 \e[33m Used Portage version is: ${PORTAGE_VERSION}"
+
 
 temporary_directory="$(mktemp --directory)"
 
 gentoo_tree_dir="${PORTAGE_ROOT}/usr/portage"
 portage_conf_dir="${PORTAGE_ROOT}/etc/portage"
-
-DISTDIR="$(mktemp --directory)"
 
 mkdir --parents "${PORTAGE_ROOT}/usr/lib64"
 ln --symbolic lib64 "${PORTAGE_ROOT}/usr/lib"
@@ -24,8 +23,27 @@ mkdir "${portage_extracted_directory}"
 curl --location --silent "https://github.com/gentoo/portage/archive/portage-${PORTAGE_VERSION}.tar.gz" | tar --extract --gzip --directory="${portage_extracted_directory}" --strip-components=1
 
 cd "${portage_extracted_directory}"
-
 ./setup.py install -O2 --system-prefix="${PORTAGE_ROOT}/usr" --sysconfdir="${PORTAGE_ROOT}/etc"
-cp cnf/metadata.dtd "${DISTDIR}/"
-
 mkdir --parents "${PORTAGE_ROOT}/usr/lib/portage/cnf/"
+
+
+curl --location --silent "https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz" | tar --extract --gzip --directory="${gentoo_tree_dir}" --strip-components=1
+
+
+mkdir -p "${portage_conf_dir}/repos.conf"
+cat > "${portage_conf_dir}/repos.conf/repos" << _EOF_
+[DEFAULT]
+main-repo = gentoo
+[gentoo]
+location = ${gentoo_tree_dir}
+
+[${TRAVIS_REPO_SLUG}]
+location = ${TRAVIS_BUILD_DIR}
+_EOF_
+
+cat > "${portage_conf_dir}/make.conf" << _EOF_
+DISTDIR="$(mktemp --directory)"
+PKGDIR="$(mktemp --directory)"
+PORTAGE_TMPDIR="$(mktemp --directory)"
+RPMDIR="$(mktemp --directory)"
+_EOF_
