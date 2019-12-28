a9e5b98870ebcd86ff94d9cb5e961c74fa38e623
expeditioneer <expeditioneer@gentoo.org>
2019-12-28 16:58:20 +0100

    underpants gnomes

--- tools/setup_portage.sh
+++ tools/setup_portage.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+
+if [ -z "${PORTAGE_ROOT}" ]; then
+  echo "PORTAGE_ROOT not set"
+  exit 1
+fi
+
 if [ -z "${PORTAGE_VERSION}" ]; then
   echo "PORTAGE_VERSION not set"
   exit 1
 fi
 
-echo -e "\e[1 \e[33m Used Portage version is: ${PORTAGE_VERSION}"
-
+echo -e "\e[33m Used Portage version is: ${PORTAGE_VERSION}"
 
 temporary_directory="$(mktemp --directory)"
 
@@ -26,7 +31,7 @@ cd "${portage_extracted_directory}"
 ./setup.py install -O2 --system-prefix="${PORTAGE_ROOT}/usr" --sysconfdir="${PORTAGE_ROOT}/etc"
 mkdir --parents "${PORTAGE_ROOT}/usr/lib/portage/cnf/"
 
-
+mkdir --parents "${gentoo_tree_dir}"
 curl --location --silent "https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz" | tar --extract --gzip --directory="${gentoo_tree_dir}" --strip-components=1
 
 
@@ -34,6 +39,7 @@ mkdir -p "${portage_conf_dir}/repos.conf"
 cat > "${portage_conf_dir}/repos.conf/repos" << _EOF_
 [DEFAULT]
 main-repo = gentoo
+
 [gentoo]
 location = ${gentoo_tree_dir}
 
@@ -47,3 +53,7 @@ PKGDIR="$(mktemp --directory)"
 PORTAGE_TMPDIR="$(mktemp --directory)"
 RPMDIR="$(mktemp --directory)"
 _EOF_
+
+ln --symbolic "${gentoo_tree_dir}/profiles/base" "${portage_conf_dir}/make.profile"
+
+rm --recursive --force "${temporary_directory}"
